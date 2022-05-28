3ba06c901bdc81ac03ad1d8d76ea3fa4621d5f89
Anna Cho <loveablessing@gmail.com>
2022-05-28 11:45:03 -0400

    commit tree 24d9e696a0c3fe836776ec43685f3e387c2ed02e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {name: "Anna", streetAddress: "Woodside"}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    let employee1 = {...obj};
+    console.log(employee1);
+    employee1[key] = value;
+    console.log(employee1);
+    return employee1;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    let deleted = {...obj};
+    delete deleted[key];
+    return deleted;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+
+
+
+
